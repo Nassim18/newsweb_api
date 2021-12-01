0e1cd541f384c736ec2cd50fbcabe20a9b4253ba
SHOUT Oussama-Nassim <Oussama-Nassim.SHOUT@akka.eu>
2021-12-01 15:49:40 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { BusinessnewsComponent } from './businessnews/businessnews.component';
+import { TechnewsComponent } from './technews/technews.component';
+import { TopheadingComponent } from './topheading/topheading.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: TopheadingComponent }, // home
+  { path: 'tech', component: TechnewsComponent },  // tech news
+  { path: 'business', component: BusinessnewsComponent }  // bus news
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TopheadingComponent } from './topheading/topheading.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsapiservicesService } from './service/newsapiservices.service';
+import { TechnewsComponent } from './technews/technews.component';
+import { BusinessnewsComponent } from './businessnews/businessnews.component';
+// for HttpClient import:
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadingComponent,
+    TechnewsComponent,
+    BusinessnewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    LoadingBarHttpClientModule
   ],
-  providers: [],
+  providers: [NewsapiservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
